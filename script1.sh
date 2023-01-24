057127c3e3960ce470467db442c5771c3988aa0d
Ahjam Taoufik <taoufikahjam@gmail.com>
2023-01-24 17:24:20 +0100

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# HOME variable 
+VAR_PATH="$HOME"
+echo "$VAR_PATH"
+ls "$VAR_PATH" 
+
+
+# USER variable
+VAR_USER="$USER"
+echo "$VAR_USER"
+
+
+# USER hostname
+VAR_HOSTNAME="$HOSTNAME"
+echo "$VAR_HOSTNAME"
+
